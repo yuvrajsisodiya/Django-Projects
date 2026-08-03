7a23a4395b7b4cef2384145986b2b42e105196a6
Yuvraj <yuvraj@gmail.com>
2026-08-03 13:12:53 +0530

    03-08-2026

---
+++ project6/student/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,re_path
+from . import views
+urlpatterns=[
+    path('',views.student,name='student'),
+    path('welcome_user/<str:username>/',views.welcome_user,name="welcome_user"),
+    path('student_detail/<int:std_id>/',views.student_detail,name="student_detail"),
+    # mutliple variable add
+    path('course_detail/<int:c_code>/<str:c_name>/',views.course_detail, name="course_detail"),
+    re_path(r'^pass_year/(?P<year>[0-9]{4})/$',views.pass_year , name="pass_year"),
+]
---
+++ project6/student/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def student(req):
+    return HttpResponse("hello student !")
+
+def welcome_user(req,username):
+    return HttpResponse(f"<h1>Welcome : {username}!</h1>")
+def student_detail(req,std_id):
+    return HttpResponse(f"<h1>Student detail With ID : {std_id}</h1>")
+
+def course_detail(req,**kwargs):
+    c_code = kwargs.get("c_code")
+    c_name = kwargs.get("c_name")
+    
+    return HttpResponse(f"Course Code : {c_code}<br>Course Name : {c_name}")
+def pass_year(req,year):
+    return HttpResponse(f"<h1>Passing Year : {year}</h1>")
