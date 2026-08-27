82594ccf0ecc57b3e1c578ef469147f83d590036
Yuvraj <yuvraj@gmail.com>
2026-08-27 13:29:53 +0530

    '27-08'

---
+++ project19/portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    age=models.IntegerField()
+    marks=models.IntegerField()
+    city=models.CharField(max_length=50)
+
+def __str__(self):
+    return self.name
+    
